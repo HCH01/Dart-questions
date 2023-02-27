7f816736f1bc69b24a942949dfd59b43a8f4df84
Himanshu Chaudhary <96414091+HCH01@users.noreply.github.com>
2023-02-28 00:49:36 +0530

    Update q2.dart

--- Assignment-2/q2.dart
+++ Assignment-2/q2.dart
@@ -1,7 +1,5 @@
 //sum of elements which is >200
 void main() {
   var li = [200, 300, 400, 133, 100, 300];
-  int sum = 0;
-  li.where((e) => e > 200).forEach((element) => sum += element);
-  print(sum);
+  print(li.where((e) => e > 200).reduce((x , y) => x + y));
 }
